339fec792c1130392f510d5433b2a05235669a1f
daniel kinny <danielkinny0214@gmail.com>
2024-12-24 23:48:28 +0000

    Add files via upload

---
+++ tribb.java
@@ -0,0 +1,24 @@
+class Main{
+
+    public static void main(String[] args){
+
+        System.out.println(algo.trib(5));
+    }
+}
+
+class algo{
+
+    static int trib(int n){
+
+        if (n==0 || n==1){ return n; }
+        else if (n==3){ return n-1; }
+        else{
+            return trib(n-1)+trib(n-2)+trib(n-3);
+        }
+    }
+}
+
+class Mathematician{
+
+    public M
+}
